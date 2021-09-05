e2d02ab4c5ce5404c33160d63ae0aaf21eb7efcf
krishnakant13 <53751675+krishnakant13@users.noreply.github.com>
2021-09-05 18:02:28 +0530

    B2B

---
+++ network/bin/_launch.sh
@@ -0,0 +1,4 @@
+#PEER_MODE=net
+#Command=dev-init.sh -e 
+#Generated: Tue Feb 23 12:57:44 UTC 2021 
+docker-compose  -f ./compose/docker-compose.base.yaml      -f ./compose/docker-compose.explorer.yaml    up -d --remove-orphans
